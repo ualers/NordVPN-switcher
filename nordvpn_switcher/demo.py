d53dd3d4f33bc58f905540825ffe5d6d65584bac
ualers <125712906+ualers@users.noreply.github.com>
2024-04-03 11:43:38 -0300

    Update demo.py

--- nordvpn_switcher/demo.py
+++ nordvpn_switcher/demo.py
@@ -100,7 +100,7 @@ for i in range(3):
     time.sleep(10)
 
 
-# [9] Now you can rotate in all states of use with this function
+# [9] Now you can rotate in all states in the USA with this function
 
 def modulo_rotation_by_usa():
     timezone_language_mapping = {
@@ -176,8 +176,131 @@ def modulo_rotation_by_usa():
             print(end_fim)
             time.sleep(1)    
 
+# [10] Now you can rotate in all states in the USA and a few more countries with this function
 
+def modulo_rotation_by_timezone():
 
+        timezone_language_mapping = {
+            "America/New_York": ["EN-US", "en"],
+            "America/Chicago": ["EN-US", "en"],
+            "America/Denver": ["EN-US", "en"],
+            "America/Los_Angeles": ["EN-US", "en"],
+            "America/Seattle": ["EN-US", "en"],
+            "America/Salt_Lake_City": ["EN-US", "en"],
+            "America/San_Francisco": ["EN-US", "en"],
+            "America/Dallas": ["EN-US", "en"],
+            "America/Kansas_City": ["EN-US", "en"],
+            "America/Saint_Louis": ["EN-US", "en"],
+            "America/Atlanta": ["EN-US", "en"],
+            "America/Charlotte": ["EN-US", "en"],
+            "America/Miami": ["EN-US", "en"],
+            "America/Manassas": ["EN-US", "en"],
+            "America/Buffalo": ["EN-US", "en"],
+            "America/Phoenix": ["EN-US", "en"],
+            "America/Argentina/Buenos_Aires": ["ES-AR", "es"],
+            "America/Sao_Paulo": ["PT-BR", "pt"],
+            "America/Mexico_City": ["ES-MX", "es"],
+            "America/Santiago": ["ES-CL", "es"],
+            "America/Toronto": ["EN-CA", "en"],
+            "America/Vancouver": ["EN-CA", "en"],
+            "Europe/London": ["EN-GB", "en"],
+            "Europe/Paris": ["FR-FR", "fr"],
+            "Europe/Madrid": ["ES-ES", "es"],
+            "Europe/Rome": ["IT-IT", "it"],
+            "Australia/Sydney": ["EN-AU", "en"],
+            "New_Zealand": ["EN-NZ", "en"],
+            "North_America/Ottawa": ["EN-CA", "en"],
+            "North_America/Havana": ["ES-CU", "es"],
+
+        }
+
+        vpn_options = {
+            "America/New_York": ["New York,Manassas"],
+            "America/Chicago": ["Chicago,Saint Louis"],
+            "America/Denver": ["Denver,Chicago"],
+            "America/Los_Angeles": ["Los Angeles,Phoenix"],
+            "America/Seattle": ["Seattle,Vancouver"],
+            "America/Salt_Lake_City": ["Salt Lake City,Denver"],
+            "America/San_Francisco": ["San Francisco,Los Angeles,Salt Lake City"],
+            "America/Dallas": ["Dallas,Chicago"],
+            "America/Kansas_City": ["Chicago,Saint Louis"],
+            "America/Saint_Louis": ["Saint Louis,Dallas"],
+            "America/Atlanta": ["Atlanta,Charlotte"],
+            "America/Charlotte": ["Charlotte,Manassas,New York"],
+            "America/Miami": ["Miami,Atlanta,Charlotte"],
+            "America/Manassas": ["Manassas,New York"],
+            "America/Buffalo": ["Buffalo,New York"],
+            "America/Phoenix": ["Phoenix,Los Angeles"],
+            "America/Argentina/Buenos_Aires": ["Argentina,Chile"],
+            "America/Sao_Paulo": ["Brazil,Argentina"],
+            "America/Mexico_City": ["Mexico,Costa rica"],
+            "America/Santiago": ["Chile,Argentina"],
+            "America/Toronto": ["Toronto,Buffalo"],
+            "America/Vancouver": ["Vancouver,Seattle"],
+            "Europe/London": ["United Kingdom,Spain"],
+            "Europe/Paris": ["Vancouver,Buffalo"],
+            "Europe/Madrid": ["Spain,Los Angeles"],
+            "Europe/Rome": ["Italy,Los Angeles"],
+            "Australia/Sydney": ["New Zealand,Sydney"],
+            "New_Zealand": ["New Zealand,Sydney"],
+            "North_America/Ottawa": ["Vancouver,Montreal"],
+            "North_America/Havana": ["Montreal,Seattle"],
+
+        }
+
+        def choose_timezone_and_language():
+            timezone = random.choice(list(timezone_language_mapping.keys()))
+            languages_stealth = timezone_language_mapping[timezone]
+            languages_lang = timezone_language_mapping[timezone][1]
+            return timezone, languages_stealth, languages_lang
+
+        timezonex, languagesx, languages_lang = choose_timezone_and_language()
+        print("Timezone selecionado:", timezonex)
+        print("Idioma do stealth:", languagesx)
+        print("Idioma do lang:", languages_lang)
+
+        vpn_options_for_timezone = vpn_options.get(timezonex, [])
+        if vpn_options_for_timezone:
+            random_a = random.random()
+            if random_a > 0.5:
+                vpn_option = random.choice(vpn_options_for_timezone)
+            else:
+                vpn_option = random.choice(vpn_options_for_timezone)
+                vpn_option = random.choice(vpn_options_for_timezone)   
+            try:
+                print(F" Rotação de ip em progresso  ")
+                vpn_instruction = initialize_VPN(area_input=[vpn_option])
+                rotate_VPN(instructions=vpn_instruction)
+                print(F" Rotação de ip concluida  ")
+                print(F" Aguardando 10 segundos ")
+                time.sleep(10)
+                return True, timezonex, languagesx, languages_lang
+            except Exception as e:
+                print(f"Erro ao conectar via VPN: {e}")
+                print("Reiniciando conexão VPN...")
+                terminate_VPN(instructions=vpn_instruction)
+                
+                timezonex, languagesx, languages_lang = choose_timezone_and_language()
+                print("Timezone 2  selecionado:", timezonex)
+                print("Idioma do stealth 2:", languagesx)
+                print("Idioma do lang 2 :", languages_lang)
+
+                vpn_options_for_timezone = vpn_options.get(timezonex, [])
+                if vpn_options_for_timezone:
+                    vpn_option = random.choice(vpn_options_for_timezone)
+                    try:
+                        print(F" Rotação de ip em progresso 2 ")
+                        vpn_instruction2 = initialize_VPN(area_input=[vpn_option])
+                        rotate_VPN(instructions=vpn_instruction2)
+                        print(F" Rotação de ip concluida  2")
+                        print(F" Aguardando 10 segundos 2 ")
+                        time.sleep(10)
+                        return True, timezonex, languagesx, languages_lang
+                    except Exception as e:
+                        print(f"Erro ao conectar via VPN: {e}")
+                        print("Reiniciando conexão VPN...")
+                        #terminate_VPN(instructions=vpn_instruction2)
+                        return False    
 ##############
 ## LINUX #####
 ##############
